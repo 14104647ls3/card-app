f0d3e451e87160bbf4cf73665387846dd84a96e3
Eddie LI <14104647ls3@gmail.com>
2025-05-24 22:43:50 -0400

    feat: backend reponses implementation

--- src/components/ui/CardCommon.tsx
+++ src/components/ui/CardCommon.tsx
@@ -23,7 +23,7 @@ export default function CardCommon({ form, fetchForms, deleteForm }: { form: For
           <div className="flex gap-3">
             <Link 
               href={`/form/${form._id}`} 
-              className="btn-primary text-ocean-blue hover:text-deep-blue font-medium text-sm flex items-center"
+              className="btn-primary text-ocean-blue hover:bg-navy font-medium text-sm flex items-center"
             >
               <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 mr-1" viewBox="0 0 20 20" fill="currentColor" style={{ transform: 'rotate(90deg)' }}>
                 <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z" />
